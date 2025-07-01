a0ae92045c40e439eea712b24f1d5d3de5cc536f
MilkyWay <milkyway8413@gmail.com>
2025-07-01 12:28:51 +0900

    add Guide

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 @echo off
+echo Starting...
 python index.py
+echo To exit the progra, please click the Close button
 pause
--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
-echo 필요한 패키지 설치 중...
+echo Installing necessary packages...
 pip install PyQt6
 pip install yt-dlp
 pip install requests
-echo 설치 완료!
+echo Done!
 pause
